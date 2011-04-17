68024930f3c2b1cd66dea04baeb30091e8174ad5
Robin Edwards <rge@cpan.org>
2011-04-17 12:48:24 +0000

    Import of RGE/Keyword-API-0.0002 from CPAN.
    gitpan-cpan-distribution: Keyword-API
    gitpan-cpan-version:      0.0002
    gitpan-cpan-path:         RGE/Keyword-API-0.0002.tar.gz
    gitpan-cpan-author:       RGE
    gitpan-cpan-maturity:     released

--- foo.pl
+++
@@ -1,3 +0,0 @@
-my $re = qr/^(\S+)(?:\[(.*)\])|(?:\{(.*)\})?$/;
-die "bang" if 'D{ "a" : 1 ]' =~ $re;
-die "correct" if 'D{ "a" : 1 }' =~ $re;
--- lib/Keyword/API.pm
+++ lib/Keyword/API.pm
@@ -1,6 +1,6 @@
 package Keyword::API;
 BEGIN {
-  $Keyword::API::VERSION = '0.0001';
+  $Keyword::API::VERSION = '0.0002';
 }
 use 5.012;
 use strict;
@@ -33,7 +33,7 @@ Keyword::API
 
 =head1 VERSION
 
-version 0.0001
+version 0.0002
 
 =head1 SYNOPSIS
 
